91f3a808b9370936b429e446403b2fe5169ad030
z00mni <65433256+junmin-Chang@users.noreply.github.com>
2021-12-31 13:50:32 +0900

    set entity

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = +process.env.APP_PORT || 3000;
+  app.setGlobalPrefix('api');
+  console.log('Port running on:', port);
+
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('CatsOnMyKnee')
+    .setDescription('내 무릎 위 고양이 API 문서')
+    .setVersion('1.0')
+    .addTag('CatsOnMyKnee')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
