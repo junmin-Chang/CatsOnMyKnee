fcc8e257f9cc560312e610c4a269d7425006cef5
z00mni <65433256+junmin-Chang@users.noreply.github.com>
2022-01-01 16:07:12 +0900

    board module & controller services

---
+++ backend/src/boards/boards.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  @Get('/')
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+  @Post('/')
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardsService.deleteBoard(id);
+  }
+  @Patch('/:id/status')
+  updateBoardStatus(@Param('id') id: string, @Body('status') status: BoardStatus): Board {
+    return this.boardsService.updateBoardStatus(id, status);
+  }
+}
