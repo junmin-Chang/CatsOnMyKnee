02f3a6924e423ffa2addd7ba4c548b171a3ce1bf
z00mni <65433256+junmin-Chang@users.noreply.github.com>
2022-01-12 13:22:55 +0900

    Jalnan font applied

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|otf)$/i,
         use: [
           {
             loader: 'file-loader',
